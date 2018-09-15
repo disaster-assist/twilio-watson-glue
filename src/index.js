59b113dcd72844d93f7a50a7baead29d2502fb61
jakebillings <jake@jakebillings.com>
2018-09-15 17:08:52 -0400

    Add: a little bit of documentation

--- src/index.js
+++ src/index.js
@@ -8,20 +8,23 @@ const {
     TWILIO_FROM_NUMBER
 } = require('./disaster-credentials/credentials');
 
-const Cloudant = require('@cloudant/cloudant');
-const AssistantV1 = require('watson-developer-cloud/assistant/v1');
-
+//Default values for when we invoke this locally on test machines
 const DEFAULT_NUMBER = '+17205562453';
 const DEFUALT_BODY = "Hi";
 
+//Pull in required libraries
+const Cloudant = require('@cloudant/cloudant');
+const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 const twilio = require('twilio');
-//Require the Twilio module and create a REST client
+
+//Initialize the Twilio module and create a REST client
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-// Set up Assistant watsonService wrapper.
+//Initialize Assistant Watson Service wrapper
 WATSON_BLOB['version'] = '2018-02-16';
 let watsonService = new AssistantV1(WATSON_BLOB);
 
+//Initialize Cloudant connection
 const cloudant = Cloudant(CLOUDANT_BLOB);
 const conversations = cloudant.db.use('conversations');
 
@@ -31,13 +34,19 @@ const conversations = cloudant.db.use('conversations');
  * function
  *
  * the OpenWhisk application that glues Twilio to our Watson Assistant
+ *
+ * NOT PRODUCTION QUALITY
+ *  - This code was written for a 24 hour hackathon
  */
 function main(params) {
+    //Use default values if we're using this on our test machines
     if (params.From == null) {
         params.From = DEFAULT_NUMBER;
         params.Body = DEFUALT_BODY;
     }
 
+    //Since this code was written for a hackathon, it's not quite "production ready".
+    //This promise wraps our chain of indented callback functions.
     return new Promise((resolve, reject) => {
         //Query Cloudant for our state information
         return conversations.find({
@@ -47,6 +56,8 @@ function main(params) {
         }, function (err, data) {
             if (err) throw err;
 
+            //If Cloudant had a conversation state for us, use it.
+            // If not, instatiate a new one
             let state;
             if (data.docs.length === 1) {
                 state = data.docs[0];
@@ -57,6 +68,8 @@ function main(params) {
                 }
             }
 
+            //Push our request into Watson assistant to generate a textual response
+            // and a new conversation state
             return watsonService.message({
                 input: {text: params.Body},
                 workspace_id: WATSON_WORKSPACE_ID,
@@ -66,6 +79,7 @@ function main(params) {
 
                 state.watsonContext = watsonResponse.context;
 
+                //Insert the new conversation state into Cloudant
                 return conversations.insert(state, function (newErr, newResult) {
                     if (newErr) throw err;
 
@@ -76,8 +90,10 @@ function main(params) {
                         console.log(message);
                     }
 
+                    //Send the response from Watson Assistant back to the sender
+                    // via the Twilio API
                     if (watsonResponse.output.text.length > 0) {
-                        client.messages
+                        return client.messages
                             .create({
                                 to: params.From,
                                 from: TWILIO_FROM_NUMBER,
