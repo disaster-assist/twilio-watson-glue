74f9862de4e99a9d311da27a9cbf47c19bade78d
jakebillings <jake@jakebillings.com>
2018-09-15 13:35:43 -0400

    Merge: kinda merge everything

--- src/index.js
+++ src/index.js
@@ -1,39 +1,62 @@
+const fs = require('fs');
+
+//Fetch our Twilio Credentials from the .gitignored file
+const {
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN,
+    WATSON_USERNAME,
+    WATSON_PASSWORD,
+    WATSON_WORKSPACE_ID
+} = JSON.parse(fs.readFileSync(__dirname + '/credentials.json'));
+
 var AssistantV1 = require('watson-developer-cloud/assistant/v1');
+
+//Require the Twilio module and create a REST client
+const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 /**
  * Hello world as an OpenWhisk action.
  */
 function main(params) {
-  var name = params.name || 'World';
+    return client.messages
+        .create({
+            to: '+7205562453',
+            from: '+18508765124',
+            body: "Tomorrow's forecast in Financial District, San Francisco is Clear",
+            mediaUrl: 'https://climacons.herokuapp.com/clear.png',
+        });
 
-  // Set up Assistant service wrapper.
-  var service = new AssistantV1({
-                                  username: params.WATSON_USERNAME, // replace with service username
-                                  password: params.WATSON_PASSWORD, // replace with service password
-                                  url: 'https://gateway.watsonplatform.net/assistant/api',
-                                  version: '2018-02-16'
-                                });
+    // Set up Assistant service wrapper.
+    var service = new AssistantV1({
+        username: WATSON_USERNAME, // replace with service username
+        password: WATSON_PASSWORD, // replace with service password
+        url: 'https://gateway.watsonplatform.net/assistant/api',
+        version: '2018-02-16'
+    });
 
-  var workspace_id = 'fc7d0a6d-6952-4377-a7fd-85d5b79f10cb'; // replace with workspace ID
+    var workspace_id = ''; // replace with workspace ID
 
-  // Start conversation with empty message.
-  service.message({
-                    input: {text: "What time is it"},
-                    workspace_id: workspace_id
-                  }, processResponse);
+    // Start conversation with empty message.
+    service.message({
+        input: {text: "What time is it"},
+        workspace_id: workspace_id
+    }, processResponse);
 
-  // Process the service response.
-  function processResponse(err, response) {
-    if (err) {
-      console.error(err); // something went wrong
-      return;
+    // Process the service response.
+    function processResponse(err, response) {
+        if (err) {
+            console.error(err); // something went wrong
+            return;
+        }
+
+        // Display the output from dialog, if any.
+        if (response.output.text.length !== 0) {
+            console.log(response.output.text[0]);
+        }
     }
 
-    // Display the output from dialog, if any.
-    if (response.output.text.length != 0) {
-      console.log(response.output.text[0]);
-    }
-  }
 }
 
-exports.main = main(process.env)
+module.exports = {
+    main
+};
