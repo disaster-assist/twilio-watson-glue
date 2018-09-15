966adf67289133bbd6561091f4c0c064337e0097
jakebillings <jake@jakebillings.com>
2018-09-15 14:35:02 -0400

    Merge: add some more in package.json too

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,10 @@
-const index = require('../src/index');
+const app = require('../src/index');
 const expect = require('chai').expect;
 
 describe('twilio-watson-glue', () => {
     describe('main()', () => {
         it('should exist', () => {
-            expect(index.main).to.exist;
+            expect(app.main).to.exist;
         });
     });
 });
