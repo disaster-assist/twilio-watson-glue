717656e88e120c4f1fa9ca87c83441691dea76d0
Aaron Hill <aa1ronham@gmail.com>
2018-09-15 14:26:22 -0400

    Add some cloudant integration

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const {
     TWILIO_AUTH_TOKEN,
     WATSON_USERNAME,
     WATSON_PASSWORD,
-    WATSON_WORKSPACE_ID
+    WATSON_WORKSPACE_ID,
+    CLOUDANT_BLOB
 } = require('./credentials');
 
+const Cloudant = require('@cloudant/cloudant');
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
 //Require the Twilio module and create a REST client
@@ -16,40 +18,71 @@ const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
  * Hello world as an OpenWhisk action.
  */
 function main(params) {
-    return client.messages
-        .create({
-            to: '+17205562453',
-            from: '+18508765124',
-            body: "Tomorrow's forecast in Financial District, San Francisco is Clear",
-            mediaUrl: 'https://climacons.herokuapp.com/clear.png',
-        });
 
     // Set up Assistant service wrapper.
     let service = new AssistantV1({
-        username: WATSON_USERNAME, // replace with service username
-        password: WATSON_PASSWORD, // replace with service password
-        url: 'https://gateway.watsonplatform.net/assistant/api',
-        version: '2018-02-16'
+                                      username: WATSON_USERNAME, // replace with service username
+                                      password: WATSON_PASSWORD, // replace with service password
+                                      url: 'https://gateway.watsonplatform.net/assistant/api',
+                                      version: '2018-02-16'
+                                  });
+
+    var cloudant = Cloudant(CLOUDANT_BLOB);
+    var conversations = cloudant.db.use('conversations');
+    conversations.find({"selector": {
+        "phone": 16034653947
+        }}, function(err, data) {
+
+        if (err != null) {
+            throw err;
+        }
+
+        service.message({
+                            input: {text: "What time is it"},
+                            workspace_id: WATSON_WORKSPACE_ID
+                        }, function(watsonErr, watsonResponse) {
+
+
+            if (err) {
+                console.error(err); // something went wrong
+                return;
+            }
+
+            console.log("Watson response:");
+            for (var message of watsonResponse.output.text) {
+                console.log(message)
+            }
+
+            var doc;
+            if (data.docs.length === 1) {
+                doc = data.docs[0];
+            } else {
+                doc = {
+                    "phone": 16034653947,
+                    "watson-context": null
+                }
+            }
+            doc["watson-context"] = watsonResponse.context;
+
+            conversations.insert(doc, function(newErr, newResult) {
+                console.log("Updated document")
+            })
+        });
+
+
     });
+    /*var result = conversations.insert({'phone': '16034653947', 'watson-context': 'test'}, function(err, result) {
+        console.log("Inserted: " + err + result);
 
-    // Start conversation with empty message.
-    service.message({
-        input: {text: "What time is it"},
-        workspace_id: WATSON_WORKSPACE_ID
-    }, processResponse);
+        return client.messages
+          .create({
+                      to: '+17205562453',
+                      from: '+18508765124',
+                      body: "Tomorrow's forecast in Financial District, San Francisco is Clear",
+                      mediaUrl: 'https://climacons.herokuapp.com/clear.png',
+                  });
+    });*/
 
-    // Process the service response.
-    function processResponse(err, response) {
-        if (err) {
-            console.error(err); // something went wrong
-            return;
-        }
-
-        // Display the output from dialog, if any.
-        if (response.output.text.length !== 0) {
-            console.log(response.output.text[0]);
-        }
-    }
 
 }
 
