ad593c762615bc139a818288d9d2c7006e202467
Aaron Hill <aa1ronham@gmail.com>
2018-09-15 21:38:29 -0400

    Fix bash script

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -1,10 +1,10 @@
 ./scripts/travis_deploy_impl.sh
 retval=$?
-if [ retval -eq 0 ]; then
+if [ $retval -eq 0 ]; then
     ./scripts/send.sh success $DISCORD_WEBHOOK
 else
     ./scripts/send.sh failure $DISCORD_WEBHOOK
-    
+
 fi
 
 exit $retval
